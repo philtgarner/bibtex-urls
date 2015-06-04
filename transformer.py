6ca411acc50f2e169cc9390ba55731bd845f6830
Phil Garner <info@philipgarner.co.uk>
2015-06-04 12:14:39 +0100

    Maintain capitalisation
    Added a few tweaks to maintain the correct capitalisation in all titles
    and authors of URLs

--- transformer.py
+++ transformer.py
@@ -23,14 +23,16 @@ else:
 	print('At least one parameter is needed, provide the BibTeX file to parse is required')
 	sys.exit(0)
 	
-#Open the database 
+#Open the database - no special encoding needed, we can assume that Mendeley has produced a clean BibTeX file
 with open(input, encoding="utf-8") as bibtex_file:
 	parser = BibTexParser()
-	parser.customization = homogeneize_latex_encoding
 	bib_database = bibtexparser.load(bibtex_file, parser=parser)
 	
 	#Loop through the entries in the BibTeX database
 	for e in bib_database.entries:
+		#If we have a title (should always be the case but check just in case) then make sure the title is in braces to maintain any capitalisation
+		if('title' in e):
+			e['title'] = '{' + e['title'] + '}'
 		#If the entry is clasified as 'misc' then look to edit it
 		if(e['type'] == 'misc'):
 			#If there is a link and no howpublished component then we can edit
@@ -39,6 +41,9 @@ with open(input, encoding="utf-8") as bibtex_file:
 				howpublished = '\\url{' + url + '}'
 				e['howpublished'] = howpublished
 				count += 1
+			#If there is an author (there always should be) then make sure the capitalisation is preserved for misc entries
+			if('author' in e):
+				e['author'] = '{' + e['author'] + '}'
 
 #Open the output database and write the modified database
 with open(output, 'w', encoding="utf-8") as bibtex_output:
